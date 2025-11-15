6a2ca3b450dc477ebb7538b6b0f2444ecee98d6f
bhagyalakshan <137475071+bhagyalakshan@users.noreply.github.com>
2025-11-16 00:22:32 +0530

    DDDD

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   ...(process.env.NODE_ENV === "production" && {
     output: "export",
-    basePath: "/Myportfolio",
+    basePath: "/Myportfolio/",
     assetPrefix: "/Myportfolio/",
   }),
 };
