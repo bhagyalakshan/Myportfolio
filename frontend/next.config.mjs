cd8096aa7a4025a3d409f8d14350ac954c118b2b
bhagyalakshan <137475071+bhagyalakshan@users.noreply.github.com>
2025-11-15 23:53:30 +0530

    git reset --hard a3551ee7

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  basePath: "/Myportfolio",
-  assetPrefix: "/Myportfolio/",
+  ...(process.env.NODE_ENV === "production" && {
+    output: "export",
+    basePath: "/Myportfolio",
+    assetPrefix: "/Myportfolio/",
+  }),
 };
 
 export default nextConfig;
